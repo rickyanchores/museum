28fd854cf31482b8e994aa0ddece4afa58f9683c
RickyAnchores <ricky23anchores@gmail.com>
2022-09-15 22:12:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from "./pages/Home/Home";
 const App = () => {
   return(
     <div className="App">
-      <h1>Museum</h1>
         <Home />
         <Gallery />
         <Footer />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
+// IMPORT IMAGES 
+import David from "../../images/DavidBW.jpeg"
+
+
 const Home = () => {
   return (
     <div className='Home'>
-        <h3>Home</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, vel iure doloremque pariatur dignissimos quis.</p>
+        <div className="left">
+            <h1>A Picture Is A Poem</h1>
+            <h1>Without Words</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, vel iure doloremque pariatur dignissimos quis.</p>
+            <button className='btn'>Read More</button>
+        </div>
+        <div className="right">
+            <img src={David} alt="David Black & White" />
+        </div>
     </div>
   )
 }
+
+
+
 
 export default Home;
