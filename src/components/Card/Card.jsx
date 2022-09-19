603a639c86d78146af3afed93c9023febd7badf6
RickyAnchores <ricky23anchores@gmail.com>
2022-09-19 19:35:21 +0100

    added imges

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (card) => {
   return (
     <div className='Card'>
         <h3>Card</h3>
+        <img src={card.image} alt="" />
         <p>Description</p>
         <button className="info">Info</button>
     </div>
--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Card from '../../components/Card/Card';
+//IMPORT IMAGES
 
-const Gallery = () => {
+import statue from "../../images/statue.avif";
+import busto from "../../images/busto.avif";
+import pharoah from "../../images/pharoah.avif";
+const Gallery = (props) => {
     return (
         <div className='Gallery'>
             <h1>Gallery</h1>
             <div className="CardContainer">
-                <Card />
-                <Card />
-                <Card />
+                <Card image={statue}/>
+                <Card image={busto}/>
+                <Card image={pharoah}/>
             </div>
         </div>
     )
